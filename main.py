a79fea2120c5aa541d72687e052abda4410806d7
AdityaDhodade <adityadhodade@gmail.com>
2023-05-24 21:40:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,67 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from flask import Flask, request
+from fastapi import FastAPI, Body
 
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+import support
+load_dotenv()
+temp = pd.read_csv("PBLFinalDatawithClusters.csv")
+
+
+# app = Flask(__name__)
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
-
+# @app.route('/get_recomm')
+# def show():
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+def read_root():
+    return {"Hello": "World"}
 
+@app.post("/get_recomm")
+async def process_string(weight_string: str):
+    print(weight_string)
+    # weight = '0.3+0.5+0+0+0+0+0+0+0+0+0+0+0+0'
+    print('something')
+    weight_vec = []
+    s = ""
+    for i in range(len(weight_string)):
+        if weight_string[i] == ' ':
+            weight_vec.append(float(s))
+            s = ""
+        else:
+            s += weight_string[i]
+    print(weight_vec)
+    garbage, clun = support.model.kneighbors (np.array(weight_vec).reshape(1,-1)) 
+    del garbage
+    clunum = clun[0][0]
+    d,index = support.nf[clunum].kneighbors (np.array(weight_vec).reshape(1,-1))
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+    print(clunum)
+    print(index)
+    
+    token = support.get_token()
+    uri_ls = []
+    token = support.get_token()
+    for i in range(5):
+        uri_ls.append(temp['uri'][index[0][i]])
+    track_names = []
+    for i in uri_ls:
+        track_names.append(support.get_track_name(token, i))
+    json_val = {}
+    count = 0
+    for i in track_names:
+        temp_json = {"name":i[0],
+                    "artist":i[1],
+                    "poster_link":i[2],
+                    "redirect_link":i[3],
+                    "preview_link": i[4]
+                    }
+        json_val[count] = temp_json
+        count += 1
+    json_val    
+        
+    print(track_names)
+    return json_val
 
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
